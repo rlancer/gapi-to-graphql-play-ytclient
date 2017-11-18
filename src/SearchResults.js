563620e80c6240f9690d23b3a4165b85667481b9
Robert Lancer <rob@collaborizm.com>
2017-11-18 01:03:47 -0500

    first

---
+++ src/SearchResults.js
@@ -0,0 +1,127 @@
+import React, {Component} from 'react';
+import {graphql} from 'react-apollo';
+import gql from 'graphql-tag';
+import InfiniteScroll from 'react-infinite-scroll-component'
+
+class SearchResults extends Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const {data} = this.props
+
+    if (data.loading) {
+      return <div style={{paddingTop: '6rem', display: 'flex', justifyContent: 'center'}}><img src='/Ripple.gif'/></div>
+    }
+
+    console.log('data', data)
+
+    const {items} = data.youtubeV3.search.list
+
+    return (
+      <InfiniteScroll hasMore={true}
+                      style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center', paddingTop: '4rem'}}
+                      next={this.props.loadMoreEntries}>
+        {items.map(item => <div key={item.id.videoId}>
+          <img style={{borderRadius: 4, margin: '1rem'}}
+               src={`https://img.youtube.com/vi/${item.id.videoId}/hqdefault.jpg`}/>
+        </div>)}
+      </InfiniteScroll>
+    );
+  }
+}
+
+const queryNext = gql`
+  query Search ($query : String, $pageToken : String) {
+      youtubeV3(key:""){
+        search{
+          list(q: $query, part:"snippet", type:"video", maxResults:14, pageToken : $pageToken ) {
+            etag
+            eventId
+            kind
+            nextPageToken
+            prevPageToken
+            regionCode
+            visitorId,
+            items{
+              id {
+                videoId
+              }
+              snippet {
+                title
+              }
+            }
+          }   
+        } 
+      }
+     }`
+
+export default graphql(gql`
+  query Search ($query : String) {
+      youtubeV3(key:""){
+        search{
+          list(q: $query, part:"snippet", type:"video", maxResults:14) {
+            etag
+            eventId
+            kind
+            nextPageToken
+            prevPageToken
+            regionCode
+            visitorId,
+            items{
+              id {
+                videoId
+              }
+              snippet {
+                title
+              }
+            }
+          }   
+        } 
+      }
+     }`,
+  {
+    options: (props) => ({
+      variables: {
+        query: props.search.trim()
+      }
+    }),
+
+    props: (args) => {
+
+      const {data} = args
+      return {
+        data,
+        loadMoreEntries: () => {
+          return data.fetchMore({
+            query: queryNext,
+            variables: {
+              pageToken: data.youtubeV3.search.list.nextPageToken,
+              query: data.variables.query
+
+            },
+            updateQuery: (previousResult, next) => {
+              return {
+                youtubeV3: {
+                  search: {
+                    // By returning `cursor` here, we update the `loadMore` function
+                    // to the new cursor.
+                    list: {
+                      nextPageToken: next.fetchMoreResult.youtubeV3.search.list.nextPageToken,
+                      // Put the new comments in the front of the list
+                      items: [...previousResult.youtubeV3.search.list.items, ...next.fetchMoreResult.youtubeV3.search.list.items],
+                    }
+                  }
+                },
+              }
+
+            }
+          })
+        }
+      }
+    }
+  })(SearchResults)
+
+
